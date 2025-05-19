9d0503c9f2e931b20c5f50826af6b9a1cd26b3ac
Supun Wimalasena <supun@hyvor.com>
2025-05-19 11:30:40 +0200

    api testing trait

---
+++ bundle/src/Testing/ApiTestingTrait.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Hyvor\Internal\Bundle\Testing;
+
+use Symfony\Component\HttpFoundation\Response;
+
+trait ApiTestingTrait
+{
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getJson(): array
+    {
+        $response = self::getClient()->getResponse();
+        $content = $response->getContent();
+        $this->assertNotFalse($content);
+        $this->assertJson($content);
+        $json = json_decode($content, true);
+        $this->assertIsArray($json);
+        return $json;
+    }
+
+    public function assertViolationCount(int $count): void
+    {
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $response = $this->getJson();
+
+        $this->assertArrayHasKey('violations', $response);
+        $this->assertIsArray($response['violations']);
+        $this->assertCount($count, $response['violations']);
+
+        $this->assertSame(
+            "Validation failed with $count violations(s)",
+            $response['message'],
+        );
+    }
+
+    public function assertHasViolation(string $property, string $message = ''): void
+    {
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $response = $this->getJson();
+
+        $this->assertArrayHasKey('violations', $response);
+        $this->assertIsArray($response['violations']);
+
+        $found = false;
+        foreach ($response['violations'] as $violation) {
+            $this->assertIsArray($violation);
+            if ($violation['property'] === $property) {
+                $found = true;
+                if ($message) {
+                    $this->assertStringContainsString($message, $violation['message']);
+                }
+            }
+        }
+
+        $this->assertTrue($found, 'Violation not found');
+    }
+
+}
