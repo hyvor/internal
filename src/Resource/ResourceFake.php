cabe591d15dc929e68f35e25670f40b19c83ae5f
Supun Wimalasena <supun@hyvor.com>
2025-11-22 00:43:33 +0100

    resources

--- src/Resource/ResourceFake.php
+++ src/Resource/ResourceFake.php
@@ -3,12 +3,13 @@
 namespace Hyvor\Internal\Resource;
 
 use Carbon\Carbon;
+use PHPUnit\Framework\Assert;
 use Symfony\Component\DependencyInjection\Container;
 
-final class ResourceFake extends Resource
+final class ResourceFake implements ResourceInterface
 {
 
-    /** @var array<array{userId: int, resourceId: int, at: ?Carbon}> */
+    /** @var array<array{organizationId: int, resourceId: int, at: ?Carbon}> */
     private array $registered = [];
 
     /** @var array<int> */
@@ -22,30 +23,29 @@ final class ResourceFake extends Resource
 
     public static function enable(): self
     {
-        $self = new self();
-        app()->singleton(Resource::class, fn() => $self);
-        return $self;
+        $fake = new self();
+        app()->singleton(Resource::class, fn() => $fake);
+        return $fake;
     }
 
-    public static function enableForSymfony(
-        Container $container
-    ): self {
-        $self = new self($container);
-        $container->set(Resource::class, $self);
-        return $self;
+    public static function enableForSymfony(Container $container): self
+    {
+        $fake = new self();
+        $container->set(ResourceInterface::class, $fake);
+        return $fake;
     }
 
     public function assertRegistered(
-        int $userId,
+        int $organizationId,
         int $resourceId,
         ?Carbon $at = null
     ): void {
         $resource = $this->getFakeFromContainer();
         $registered = false;
 
-        foreach ($resource->registered as $registered) {
+        foreach ($this->registered as $registered) {
             if (
-                $registered['userId'] === $userId &&
+                $registered['organizationId'] === $organizationId &&
                 $registered['resourceId'] === $resourceId &&
                 ($at === null || $registered['at']?->getTimestamp() === $at->getTimestamp())
             ) {
@@ -54,30 +54,28 @@ final class ResourceFake extends Resource
             }
         }
 
-        \PHPUnit\Framework\Assert::assertTrue(
+        Assert::assertTrue(
             $registered,
-            "Resource not registered for user $userId and resource $resourceId" . ($at ? " at $at" : '')
+            "Resource not registered for user $organizationId and resource $resourceId" . ($at ? " at $at" : '')
         );
     }
 
     public function assertDeleted(int $resourceId): void
     {
-        $resource = $this->getFakeFromContainer();
-
-        \PHPUnit\Framework\Assert::assertContains(
+        Assert::assertContains(
             $resourceId,
-            $resource->deleted,
+            $this->deleted,
             "Resource not deleted: $resourceId"
         );
     }
 
     public function register(
-        int $userId,
+        int $organizationId,
         int $resourceId,
         ?Carbon $at = null
     ): void {
         $this->registered[] = [
-            'userId' => $userId,
+            'organizationId' => $organizationId,
             'resourceId' => $resourceId,
             'at' => $at,
         ];
