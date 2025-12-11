b664b287abafcbe2c95ab0df1c1e632abcfd1134
Supun Wimalasena <supun@hyvor.com>
2025-12-11 06:38:15 +0100

    wip

--- bundle/src/Command/Dev/DevResetCommand.php
+++ bundle/src/Command/Dev/DevResetCommand.php
@@ -44,7 +44,7 @@ class DevResetCommand extends Command
 
         $application->run(
             new ArrayInput([
-                'command' => 'doctrine:query:sql',
+                'command' => 'dbal:run-sql',
                 'sql' => "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = current_database() AND pid <> pg_backend_pid();",
             ]),
             $output
