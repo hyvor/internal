63a05f925d6411feac2d44e52b33e0d03d9d65ae
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2026-09-17 23:36:28 +0530

    use setError of AbstractEvent

--- bundle/src/Comms/Event/ToCore/Billing/CreateSubscriptionResponse.php
+++ bundle/src/Comms/Event/ToCore/Billing/CreateSubscriptionResponse.php
@@ -6,25 +6,12 @@ readonly class CreateSubscriptionResponse
 {
 
     public function __construct(
-        private bool $success,
-        private ?int $subscriptionId,
-        private ?string $errorMessage,
+        private int $subscriptionId,
     ) {
     }
 
-    public function isSuccess(): bool
-    {
-        return $this->success;
-    }
-
-    public function getSubscriptionId(): ?int
+    public function getSubscriptionId(): int
     {
         return $this->subscriptionId;
     }
-
-    public function getErrorMessage(): ?string
-    {
-        return $this->errorMessage;
-    }
-
 }
